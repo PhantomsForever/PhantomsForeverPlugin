257a839e1178041c5823a41d438484f1a533e213
LifeCoder <lifecoder@tutanota.com>
2018-02-21 00:41:20 -0800

    Tried implementing python sum and checksum
    Not sure this will work

--- PhantomsForever Plugin/Core/Packets/PRUdpPacket.cs
+++ PhantomsForever Plugin/Core/Packets/PRUdpPacket.cs
@@ -1,6 +1,9 @@
-﻿using PhantomsForever_Plugin.Core.Networking;
+﻿using PhantomsForever_Plugin.Core.Extensions;
+using PhantomsForever_Plugin.Core.Networking;
+using PhantomsForever_Plugin.Core.Utilities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -11,10 +14,31 @@ namespace PhantomsForever_Plugin.Core.Packets
     public class PRUdpPacket
     {
         private byte[] PacketBytes;
+        private byte[] CheckSum;
         public PRUdpPacket(byte[] data)
         {
             PacketBytes = data;
 
+        }
+        public byte[] CalculateChecksum(byte[] b = null)
+        {
+            PacketBytes = Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(PacketBytes).ljust((PacketBytes.Length + 3) & ~3, '\0'));
+            var words = Struct.Unpack(string.Format("<{0}il", Math.Truncate((decimal)PacketBytes.Length / 4)), PacketBytes);
+            try
+            {
+                var result = BitConverter.GetBytes((int)Sum.Do(words) & 0xffffffff);
+                var rarr = new byte[b.Length + result.Length];
+                using (var _mem = new MemoryStream(rarr))
+                {
+                    _mem.Write(b, 0, b.Length);
+                    _mem.Write(result, 0, result.Length);
+                    return _mem.ToArray();
+                }
+            }
+            catch(Exception)
+            {
+                return BitConverter.GetBytes((int)Sum.Do(words) & 0xffffffff);
+            }
         }
         public void Send(PRUdpServer server, IPEndPoint ep)
         {
---
+++ PhantomsForever Plugin/Core/Utilities/Sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhantomsForever_Plugin.Core.Utilities
+{
+    public class Sum
+    {
+        public static object Do(object[] arr, int start = 0)
+        {
+            return arr.Length + start;
+        }
+        public static string Do(string[] arr, int start = 0)
+        {
+            return Convert.ToString(arr.Length + start);
+        }
+    }
+}
